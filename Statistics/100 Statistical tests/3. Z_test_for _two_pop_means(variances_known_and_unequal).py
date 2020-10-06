02f13a1c6f05b1c87c39869fb3fdf023183a6f54
Sharukkhan <54264338+Sharukkhan777@users.noreply.github.com>
2020-10-06 13:51:50 +0530

    Add files via upload

---
+++ Statistics/100 Statistical tests/3. Z_test_for _two_pop_means(variances_known_and_unequal).py
@@ -0,0 +1,71 @@
+# 2. Statistical test
+# Z- test for a population mean(variance known and equal)
+
+import scipy.stats as st
+import statistics as stat
+import math as m
+inp = input("Press 1 for parameter values known\nPress 2 for data(series) known\n")
+
+if inp == "1":
+    n1 = float(input("n1 = "))
+    n2 = float(input("n2 = "))
+    x_bar1 = float(input("x_bar1 = "))
+    x_bar2 = float(input("x_bar2 = "))
+    sigma1 = float(input("sigma1 = "))
+    sigma2 = float(input("sigma2 = "))
+    on_tw = input("Press 1 for one tail\nPress 2 for two tail\n")
+    los = float(input("level_of_sig = "))
+    z_cal = (x_bar1 - x_bar2)/(( (pow(sigma1,2)/n1)+(pow(sigma2,2)/n2) )**0.5)
+    # calculate the z_table value
+    if on_tw == 1:
+        cl = st.norm.ppf(1 - los)
+        if z_cal < cl:
+            print("Null Hypothesis accepted:")
+            print("No Significant difference between assumed pop mean and sample mean")
+        else:
+            print("Null Hypothesis Rejected:")
+            print("Significant difference between assumed pop mean and sample mean")
+        
+    else:
+        lower_cl = st.norm.ppf(0+(los/2))
+        upper_cl = st.norm.ppf(1-(los/2))
+        if (z_cal > lower_cl) and (z_cal < upper_cl):
+            print("Null Hypothesis accepted:")
+            print("No Significant difference between assumed pop mean and sample mean")
+        else:
+            print("Null Hypothesis Rejected:")
+            print("Significant difference between assumed pop mean and sample mean")
+    
+else:
+    data1 = eval(input("Input your data1 in list format, [1,2,3]\n"))
+    data2 = eval(input("Input your data2 in list format, [1,2,3]\n"))
+    n1 = len(data1)
+    n2 = len(data2)
+    x_bar1 = stat.mean(data1)
+    x_bar2 = stat.mean(data2)
+    sigma1 = float(input("sigma1 = "))
+    sigma2 = float(input("sigma2 = "))
+    on_tw = input("Press 1 for one tail\nPress 2 for two tail\n")
+    los = float(input("level_of_sig = "))
+    z_cal = (x_bar1 - x_bar2)/(( (pow(sigma1,2)/n1)+(pow(sigma2,2)/n2) )**0.5)
+    # calculate the z_table value
+    if on_tw == 1: 
+        cl = st.norm.ppf(1 - los)
+        if z_cal < cl:
+            print("Null Hypothesis accepted:")
+            print("No Significant difference between means of two population")
+        else:
+            print("Null Hypothesis Rejected:")
+            print("Significant difference between means of two population")
+        
+    else:
+        lower_cl = st.norm.ppf(0+(los/2))
+        upper_cl = st.norm.ppf(1-(los/2))
+        if (z_cal > lower_cl) and (z_cal < upper_cl):
+            print("Null Hypothesis accepted:")
+            print("No Significant difference between means of two population")
+        else:
+            print("Null Hypothesis Rejected:")
+            print("Significant difference between means of two population")
+    
+    
